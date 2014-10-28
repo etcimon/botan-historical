1660697bf03d7138e7ade2f0956b3e4e3274c377
Etienne Cimon <etcimon@gmail.com>
2014-10-28 17:06:14 -0400

    Added more intrinsics

--- source/botan/utils/simd/immintrin.d
+++ source/botan/utils/simd/immintrin.d
@@ -15,6 +15,11 @@ pure:
 nothrow:
 @trusted:
 
+int _MM_SHUFFLE(int a, int b, int c, int d)
+{
+	return (z<<6) | (y<<4) | (x<<2) | w;
+}
+
 version(GDC) {
 	// GDC <--> immintrin => gcc/gcc/config/i386/immintrin.h
 	static import gcc.attribute;
@@ -40,7 +45,7 @@ version(GDC) {
 
 	@inline @avx2
 	__m256i _mm256_set_epi64x(long a, long b, long c, long d) {
-		return cast(__m256i) long4(a, b, c, d);
+		return cast(__m256i) long4([a, b, c, d]);
 	}
 	
 	@inline @avx2
@@ -103,7 +108,7 @@ version(LDC) {
 		int _rdrand32_step(uint*);
 
 	__m256i _mm256_set_epi64x(long a, long b, long c, long d) {
-		return cast(__m256i) long4(a, b, c, d);
+		return cast(__m256i) long4([a, b, c, d]);
 	}
 
 	__m256i _mm256_unpacklo_epi64(__m256i a, __m256i b) {
@@ -139,11 +144,13 @@ version(LDC) {
 		
 	}
 	
-	__m256i _mm256_permute4x64_epi64(__m256i a) {
+	__m256i _mm256_permute4x64_epi64(__m256i a, in int M) {
 		pragma(LDC_allow_inline);
-		return inlineIR!(`%tmp = shufflevector %0 <i32 3, i32 0, i32 2, i32 0>
+
+		int[4] val = [(M) & 0x3, ((M) & 0xc) >> 2, ((M) & 0x30) >> 4, ((M) & 0xc0) >> 6];
+		return inlineIR!(`%tmp = shufflevector <4 x i64> %0, <4 x i64> undef, <i32 %1, i32 %2, i32 %3, i32 %4>
 		       			  ret <4 x i64> %tmp`,
-		                 __m256i)(a);		
+		                 __m256i)(a, val[0], val[1], val[2], val[3]);		
 	}
 	
 	__m256i _mm256_add_epi64(__m256i a, __m256i b) {
@@ -174,38 +181,147 @@ version(LDC) {
 		                 __m256i)(a, b);
 	}
 
-	__m256i _mm256_or_si256(__m256i a, __m256i b) {
-		pragma(LDC_allow_inline);
-		return inlineIR!(`%tmp = or <4 x i64> %0, %1
-		       			  ret <4 x i64> %tmp`,
-		                 __m256i)(a, b);
-	}
-
 	pragma(LDC_intrinsic, "llvm.x86.avx2.psrlv.q.256")
 		__m256i _mm256_srlv_epi64(__m256i a, __m256i b);
 
 	pragma(LDC_intrinsic, "llvm.x86.avx2.psllv.q.256")
-		__m256i _mm256_srlv_epi64(__m256i a, __m256i b);
+		__m256i _mm256_sllv_epi64(__m256i a, __m256i b);
+
 
 
 }
 
 version(DMD) {
-	void store(__m128i* src, __m128i* dst) {
+	int _rdrand32_step(uint* r) {
+		int ret;
+
 		asm
 		{
-			mov RAX, src;
-			mov RBX, dst;
-			movdqu XMM0, [RAX];
-			movdqu [RBX], XMM0;
+			mov EAX, ret;
+			rdrand EAX;
+			mov ret, EAX;
 		}
+		if (ret != 0)
+			*r = ret;
+		else
+			return 0;
+		return 1;
 	}
+
+	__m256i _mm256_unpacklo_epi64(__m256i a, __m256i b) 
+	{
+		// http://www.felixcloutier.com/x86/PUNPCKLBW:PUNPCKLWD:PUNPCKLDQ:PUNPCKLQDQ.html
+
+		__m256i ret;
+
+		__m256i* _a = &a;
+		__m256i* _b = &b;
+		__m256i* _c = &ret;
+		
+		asm 
+		{
+			mov RAX, _a;
+			mov RBX, _b;
+			mov RCX, _c;
+			vpunpcklqdq [RCX], [RAX], [RBX]; 
+		}
+		
+		return ret;
+		
+	}
+
+	__m256i _mm256_unpackhi_epi64(__m256i a, __m256i b) 
+	{
+		// http://www.felixcloutier.com/x86/PUNPCKHBW:PUNPCKHWD:PUNPCKHDQ:PUNPCKHQDQ.html
+
+		__m256i ret;
+
+		__m256i* _a = &a;
+		__m256i* _b = &b;
+		__m256i* _c = &ret;
+		
+		asm 
+		{
+			mov RAX, _a;
+			mov RBX, _b;
+			mov RCX, _c;
+			vpunpckhqdq [RCX], [RAX], [RBX];
+		}
+		
+		return ret;
+		
+	}
+
+	__m256i _mm256_set_epi64x(long a, long b, long c, long d) {
+		return cast(__m256i) long4([a, b, c, d]);
+	}
+	
+	__m256i _mm256_loadu_si256(__m256i* a) 
+	{
+		// http://www.felixcloutier.com/x86/MOVDQU.html
+
+		__m256i ret;
+		__m256i* b = &ret;
+		asm 
+		{
+			mov RAX, a;
+			mov RBX, b;
+			vmovdqu YMM0, [RAX];
+			vmovdqu [RBX], YMM0;
+		}
+		
+		return ret;
+		
+	}
+
+	void _mm256_storeu_si256(__m256i* ptr, __m256i a) {
+
+		__m256i ret;
+		__m256i* _a = &a;
+		__m256i* _b = &ret;
+		asm 
+		{
+			mov RAX, _a;
+			mov RBX, _b;
+			vmovdqu YMM0, [RAX];
+			vmovdqu [RBX], YMM0;
+		}
+
+		*ptr = ret;
+				
+	}
+
+	__m256i _mm256_permute4x64_epi64(__m256i a, in int M) {
+		__m256i ret;
+		__m256i* _a = &a;
+		__m256i* _b = &ret;
+		ubyte[4] val = [cast(ubyte) ((M) & 0x3), cast(ubyte) (((M) & 0xc) >> 2), cast(ubyte) (((M) & 0x30) >> 4), cast(ubyte) (((M) & 0xc0) >> 6)];
+
+		ubyte _imm8;
+		_imm8 |= (val >> 0) & 0x3;
+		_imm8 |= (val >> 2) & 0x3;
+		_imm8 |= (val >> 4) & 0x3;
+		_imm8 |= (val >> 6) & 0x3;
+
+		asm 
+		{
+			mov imm8, _imm8;
+			mov RAX, _a;
+			mov RBX, _b;
+			vmovdqu YMM0, [RAX];
+			vmovdqu [RBX], YMM0;
+		}
+		
+		*ptr = ret;	
+	}
+
 }
 
 // _mm256_unpacklo_epi64
 // _mm256_unpackhi_epi64
 // _mm256_set_epi64x
 // _mm256_loadu_si256
+// _mm256_storeu_si256
 // _mm256_permute4x64_epi64
 // _mm256_add_epi64
 // _mm256_sub_epi64
